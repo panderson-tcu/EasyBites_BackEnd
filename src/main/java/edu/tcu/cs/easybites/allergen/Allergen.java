8ee296946ca682645ac59524806c5bd0257f0476
Paige Anderson <panderson.pdx@gmail.com>
2023-11-14 22:00:22 -0600

    add allergen, appliance, and recipe fields

--- src/main/java/edu/tcu/cs/easybites/allergen/Allergen.java
+++ src/main/java/edu/tcu/cs/easybites/allergen/Allergen.java
@@ -1,9 +1,37 @@
 package edu.tcu.cs.easybites.allergen;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.io.Serializable;
 
 @Entity
 public class Allergen implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer allergenId;
+
+    private String name;
+
+    public Allergen() {
+    }
+
+    public Integer getAllergenId() {
+        return allergenId;
+    }
+
+    public void setAllergenId(Integer allergenId) {
+        this.allergenId = allergenId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
