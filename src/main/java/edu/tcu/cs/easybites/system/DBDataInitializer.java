f49fd0da3c5ca4997d9de56b5a2c28b5ace235fc
fjosea10 <fjosea10@gmail.com>
2023-11-17 14:31:34 -0600

    added DBDataInitializer class

---
+++ src/main/java/edu/tcu/cs/easybites/system/DBDataInitializer.java
@@ -0,0 +1,11 @@
+package edu.tcu.cs.easybites.system;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DBDataInitializer implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
