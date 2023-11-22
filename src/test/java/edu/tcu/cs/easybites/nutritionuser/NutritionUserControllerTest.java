b83dce2c56f3c58db47097d6c032d103ae693766
fjosea10 <fjosea10@gmail.com>
2023-11-22 10:16:40 -0600

    added annotations

--- src/test/java/edu/tcu/cs/easybites/nutritionuser/NutritionUserControllerTest.java
+++ src/test/java/edu/tcu/cs/easybites/nutritionuser/NutritionUserControllerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +24,8 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class NutritionUserControllerTest {
 
     @Autowired
