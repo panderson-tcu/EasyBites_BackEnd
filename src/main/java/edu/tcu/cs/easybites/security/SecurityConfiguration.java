b53fff18fc8c46c34688e9747ef301cfc59fdff1
fjosea10 <fjosea10@gmail.com>
2024-03-19 21:57:56 -0500

    changed security config to authenticate clerk jwt with our spring security backend

--- src/main/java/edu/tcu/cs/easybites/security/SecurityConfiguration.java
+++ src/main/java/edu/tcu/cs/easybites/security/SecurityConfiguration.java
@@ -1,11 +1,18 @@
 package edu.tcu.cs.easybites.security;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.KeySourceException;
 import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import com.nimbusds.jwt.proc.JWTProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +24,7 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -26,11 +34,17 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
 
 @Configuration
 public class SecurityConfiguration {
@@ -63,32 +77,38 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       return http
+        return http
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        // All recipe API endpoints should require authentication
-                        // Here we only specify PUT - update recipe status for admins only
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.PUT, this.baseUrl + "/recipes/{newStatus}/{recipeId}")).hasAuthority("ROLE_admin")
+                                // All recipe API endpoints should require authentication
+                                // Here we only specify PUT - update recipe status for admins only
+                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.PUT, this.baseUrl + "/recipes/{newStatus}/{recipeId}")).hasAuthority("ROLE_admin")
 
-                        // nutrition-user api endpoints
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.PUT, this.baseUrl + "/nutrition-user/**")).hasAuthority("ROLE_admin")
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, this.baseUrl + "/nutrition-user")).hasAuthority("ROLE_admin") // login does not need admin
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.DELETE, this.baseUrl + "/nutrition-user/**")).hasAuthority("ROLE_admin")
-                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, this.baseUrl + "/nutrition-user/**")).hasAuthority("ROLE_admin")
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll() // h2 console
+                                // nutrition-user api endpoints
+                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.PUT, this.baseUrl + "/nutrition-user/**")).hasAuthority("ROLE_admin")
+                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, this.baseUrl + "/nutrition-user")).hasAuthority("ROLE_admin") // login does not need admin
+                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.DELETE, this.baseUrl + "/nutrition-user/**")).hasAuthority("ROLE_admin")
+                                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, this.baseUrl + "/nutrition-user/**")).hasAuthority("ROLE_admin")
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll() // h2 console
 
-                        .anyRequest().authenticated() // always good idea to put this as last to authenticate everything else.
+                                .anyRequest().authenticated() // always good idea to put this as last to authenticate everything else.
 //                                .anyRequest().permitAll()
 
                 )
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .csrf(AbstractHttpConfigurer::disable)
-               .cors(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(this.customBasicAuthenticationEntryPoint))
-               .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(
-                       jwt -> jwt.decoder(jwtDecoder())
-                               .jwtAuthenticationConverter(jwtAuthenticationConverter())
-               ).authenticationEntryPoint(this.customBearerTokenAuthenticationEntryPoint)
-                       .accessDeniedHandler(this.customBearerTokenAccessDeniedHandler))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(
+                                jwt -> {
+                                    try {
+                                        jwt.decoder(jwtDecoder())
+                                                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+                                    } catch (MalformedURLException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                }
+                        ).authenticationEntryPoint(this.customBearerTokenAuthenticationEntryPoint)
+                        .accessDeniedHandler(this.customBearerTokenAccessDeniedHandler))
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
@@ -106,11 +126,44 @@ public class SecurityConfiguration {
         return new NimbusJwtEncoder(jwkSet);
     }
 
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        // use public key to decode and verify. this public key can be distributed, but private key must be kept secret
+//        return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
+//    }
+
+
     @Bean
-    public JwtDecoder jwtDecoder() {
-        // use public key to decode and verify. this public key can be distributed, but private key must be kept secret
-        return NimbusJwtDecoder.withPublicKey(this.publicKey).build();
+    public JwtDecoder jwtDecoder() throws MalformedURLException {
+        // Load Clerk's public keys
+        JWKSource<SecurityContext> clerkJwkSource = new RemoteJWKSet<>(new URL("https://precious-skink-82.clerk.accounts.dev/.well-known/jwks.json"));
+
+        // Load self-generated public keys
+        JWK selfGeneratedJwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
+        JWKSource<SecurityContext> selfGeneratedJwkSource = new ImmutableJWKSet<>(new JWKSet(selfGeneratedJwk));
+
+        // Combine both sources
+        JWKSource<SecurityContext> combinedJwkSource = (jwkSelector, securityContext) -> {
+            List<JWK> combinedJwks = new ArrayList<>();
+            List<JWK> clerkJwk = clerkJwkSource.get(jwkSelector, securityContext);
+            if (clerkJwk != null) {
+                combinedJwks.addAll(clerkJwk);
+            }
+            combinedJwks.addAll(selfGeneratedJwkSource.get(jwkSelector, securityContext));
+            return combinedJwks;
+        };
+
+        // Create a JWT processor
+        JWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
+        JWSVerificationKeySelector<SecurityContext> keySelector = new JWSVerificationKeySelector<>(JWSAlgorithm.RS256, combinedJwkSource);
+        ((DefaultJWTProcessor<SecurityContext>) jwtProcessor).setJWSKeySelector(keySelector);
+
+        // Create a JWT decoder
+        NimbusJwtDecoder jwtDecoder = new NimbusJwtDecoder(jwtProcessor);
+
+        return jwtDecoder;
     }
+
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
@@ -126,3 +179,4 @@ public class SecurityConfiguration {
     }
 
 }
+
